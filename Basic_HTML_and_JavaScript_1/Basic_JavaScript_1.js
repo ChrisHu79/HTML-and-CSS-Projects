68656245bd279c2e957d6582bd42c491af51dbe2
ChrisHu79 <chrishumble.uk@outlook.com>
2022-05-28 09:25:02 +0100

    Add new folder
    Added basic html and javascript folder.

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("This is an alert box written in Javascript");
+function My_First_Function() {
+var String = "Kiss Me, I'm Irish!";
+var result = String.fontcolor("green");
+document.getElementById("Irish").innerHTML =
+result;
+}
